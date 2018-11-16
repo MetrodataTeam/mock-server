533b8c5aff368edeb36e0ea3c2a26adeebc0f552
Deo <liangzhanzhao@metrodata.cn>
2018-11-16 14:15:24 +0800

    fix tests and drop python2 support

--- mock_server/tests/test_restapi.py
+++ mock_server/tests/test_restapi.py
@@ -1,80 +1,82 @@
-# -*- coding: utf-8 -*-
-
+import json
 import os
 import subprocess
 import time
 
 import tornado.testing
+
 from mock_server.application import Application
 
 
 class TestRestApi(tornado.testing.AsyncHTTPTestCase):
 
-    def setUp(self):
-        super(TestRestApi, self).setUp()
-        self._upstream_server = subprocess.Popen(
-            "mock_server/tests/upstream_server.py")
-        time.sleep(2 / 10.0)
+  @classmethod
+  def setUpClass(cls):
+    cls._upstream_server = subprocess.Popen(
+        "mock_server/tests/upstream_server.py")
+    time.sleep(2)
 
-    def tearDown(self):
-        super(TestRestApi, self).tearDown()
-        self._upstream_server.terminate()
+  def assertJsonBody(self, response, expect):
+    actual = json.loads(response.body)
+    self.assertEqual(actual, expect)
 
-    def get_app(self):
-        dirname = os.path.dirname(__file__)
+  @classmethod
+  def tearDownClass(cls):
+    cls._upstream_server.terminate()
 
-        return Application(
-            7777, "localhost", os.path.join(dirname, "api/"),
-            False, "application.json", os.path.join(dirname, "provider.py"))
+  def get_app(self):
+    dirname = os.path.dirname(__file__)
 
-    def get_new_ioloop(self):
-        return tornado.ioloop.IOLoop.instance()
+    return Application(
+        7777, "localhost", os.path.join(dirname, "api/"),
+        False, "application.json", os.path.join(dirname, "provider.py"))
 
-    def test_list_users(self):
-        response = self.fetch("/user")
+  def get_new_ioloop(self):
+    return tornado.ioloop.IOLoop.instance()
 
-        self.assertFalse(response.error)
-        self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, '["john", "tom"]\n')
+  def test_list_users(self):
+    response = self.fetch("/user")
 
-    def test_user_doesnt_exists_with_custom_header(self):
-        response = self.fetch("/user/tom?__statusCode=404")
+    self.assertFalse(response.error)
+    self.assertEqual(response.code, 200)
+    self.assertJsonBody(response, ['john', 'tom'])
 
-        self.assertEqual(response.code, 404)
-        self.assertEqual(response.body,
-                         '{"message": "User doesn\'t exists"}\n')
-        self.assertEqual(response.headers["MyCustomHeader"], "test")
+  def test_user_doesnt_exists_with_custom_header(self):
+    response = self.fetch("/user/tom?__statusCode=404")
 
-    def test_noexists_method(self):
-        response = self.fetch("/user/john/data")
+    self.assertEqual(response.code, 404)
+    self.assertJsonBody(response, {'message': 'User doesn\'t exists'})
+    self.assertEqual(response.headers["MyCustomHeader"], "test")
 
-        self.assertEqual(response.code, 404)
-        self.assertEqual(
-            response.body,
-            'Api does\'t exists, '
-            '<a href="/__manage/create?url_path=/user/'
-            'john/data&method=GET&status_code=200&format=json">'
-            'create resource method</a>')
+  def test_noexists_method(self):
+    response = self.fetch("/user/john/data")
 
-    def test_url_with_variables(self):
-        response = self.fetch("/user/lisa/family/bart")
+    self.assertEqual(response.code, 404)
+    self.assertEqual(
+        response.body.decode(),
+        'Api does\'t exists, '
+        '<a href="/__manage/create?url_path=/user/'
+        'john/data&method=GET&status_code=200&format=json">'
+        'create resource method</a>')
 
-        self.assertEqual(response.code, 200)
-        self.assertTrue("bart" in response.body)
+  def test_url_with_variables(self):
+    response = self.fetch("/user/lisa/family/bart")
 
-    def test_hello_on_upstream_server(self):
-        response = self.fetch("/hello")
+    self.assertEqual(response.code, 200)
+    self.assertTrue("bart" in response.body.decode())
 
-        self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, "Hello from upstream server")
+  def test_hello_on_upstream_server(self):
+    response = self.fetch("/hello")
 
-    def test_custom_provider(self):
-        response = self.fetch("/abc")
+    self.assertEqual(response.code, 200)
+    self.assertEqual(response.body.decode(), "Hello from upstream server")
 
-        self.assertEqual(response.code, 200)
-        self.assertEqual(
-            response.body,
-            '{"surname": "Hanacek", "name": "Tomas"}')
-        self.assertEqual(
-            response.headers["Content-Type"],
-            "application/json; charset=utf-8")
+  def test_custom_provider(self):
+    response = self.fetch("/abc")
+
+    self.assertEqual(response.code, 200)
+    self.assertJsonBody(response,
+                        {"surname": "Hanacek", "name": "Tomas"})
+    self.assertEqual(
+        response.headers["Content-Type"],
+        "application/json; charset=utf-8")
